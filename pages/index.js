aeb091ad4eeb322ad0a4f9e3d50c8c1e100b12ed
imaeumx <paghenson.student@ua.edu.ph>
2025-08-19 13:44:15 +0800

    Updated Introduction

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I am Patricia Alizah Henson. I am a billionaire.</p>
+        <p>Hello, I am Patricia Alizah Henson. I am a gazillionaire.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
